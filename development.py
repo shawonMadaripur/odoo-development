6fff9be15033096f5fc342dc1b83e5338e6d4202
shawon <saifulislamshawon1234@gmail.com>
2026-02-28 16:27:53 +0600

    odoo structure

---
+++ development.py
@@ -0,0 +1,24 @@
+project_directory/
+    |
+    |--module_a/
+    |   |--controllers/
+    |   |   |--__init__.py
+    |   |   |--controller.py
+    |   |
+    |   |--data/
+    |   |   |--data.xml
+    |   |
+    |   |--models/
+    |   |   |--__init__.py
+    |   |   |--model.py
+    |   |
+    |   |--security/
+    |   |   |--ir.model.access.csv
+    |   |
+    |   |--views/
+    |   |   |
+    |   |   |--templates.xml
+    |   |   |--views.xml
+    |   |
+    |   |--__init__.py
+    |   |--__manifest__.py
